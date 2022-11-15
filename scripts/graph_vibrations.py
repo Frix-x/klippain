783df204da912ee6e2f40f9e1698b6c527742937
sharmadhiraj86 <45898027+sharmadhiraj86@users.noreply.github.com>
2022-11-15 16:01:16 +0530

    small typo in doc (#43)
    fix and close #41

--- scripts/graph_vibrations.py
+++ scripts/graph_vibrations.py
@@ -11,7 +11,7 @@
 #   v1.0: first version of the script
 
 
-# Be sure to make this script executable using SSH: type 'chmod +x ./graph_vibrations.sh' when in the folder !
+# Be sure to make this script executable using SSH: type 'chmod +x ./graph_vibrations.py' when in the folder !
 
 #####################################################################
 ################ !!! DO NOT EDIT BELOW THIS LINE !!! ################
