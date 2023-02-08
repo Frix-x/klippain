3835a148b595c5e74724da9d0d4a42939fba3b28
Félix Boisselier <felix@fboisselier.fr>
2023-02-08 17:14:32 +0100

    fixed install script and better backup management with version tracking

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@
 #   v1.0: first version of the script to allow a peaceful install and update ;)
 
 
-set -eu
-
 # Where the user Klipper config is located (ie. the one used by Klipper to work)
 USER_CONFIG_PATH="$(realpath -e ${HOME}/printer_data/config)"
 # Where to clone Frix-x repository config files (read-only and keep untouched)
 FRIX_CONFIG_PATH="${HOME}/frix-x_config"
-# Path used to store backups when updating (automatically dated)
-BACKUP_PATH="${HOME}/.klipper_config_backup_$(date +'%Y%m%d%H%M%S')"
+# Path used to store backups when updating (backups are automatically dated when saved inside)
+BACKUP_PATH="${HOME}/frix-x_config_backups"
 
+
+set -eu
 
 # Step 1: Verify that the script is not run as root and Klipper is installed
 function preflight_checks {
@@ -42,9 +42,14 @@ function check_download {
 
     if [ ! -d "${FRIX_CONFIG_PATH}" ]; then
         echo "Downloading Frix-x configuration folder..."
-        git -C $frixtemppath clone https://github.com/Frix-x/klipper-voron-V2.git $frixreponame
-        chmod +x ${FRIX_CONFIG_PATH}/install.sh
-        echo "Download complete!"
+        
+        if git -C $frixtemppath clone https://github.com/Frix-x/klipper-voron-V2.git $frixreponame; then
+            chmod +x ${FRIX_CONFIG_PATH}/install.sh
+            echo "Download complete!"
+        else
+            echo "Download of Frix-x configuration folder from https://github.com/Frix-x/klipper-voron-V2.git failed!"
+            exit -1
+        fi 
     else
         echo "Frix-x configuration folder found!"
     fi
@@ -52,8 +57,13 @@ function check_download {
 
 # Step 3: Backup the old Klipper configuration
 function backup_config {
-    echo "Backup your old configuration files... You will find them in: ${BACKUP_PATH}"
-    mv -f ${USER_CONFIG_PATH} ${BACKUP_PATH}
+    echo "Backup your old configuration files... You will find them in: ${BACKUP_DIR}"
+    if [ ! -d "${BACKUP_PATH}" ]; then
+        mkdir ${BACKUP_PATH}
+    fi
+    mv -f ${USER_CONFIG_PATH}/*.cfg ${BACKUP_DIR}
+    mv -f ${USER_CONFIG_PATH}/*.conf ${BACKUP_DIR}
+    mv -f ${USER_CONFIG_PATH}/.VERSION ${BACKUP_DIR}
 }
 
 # Step 4: Put the new configuration files in place to be ready to start
@@ -67,15 +77,15 @@ function install_config {
     done
 
     # Copy the ADXL results from the last backup to restore them to the user's config directory
-    if [ ! -d "${BACKUP_PATH}/adxl_results" ]; then
-        cp -fa ${BACKUP_PATH}/adxl_results ${USER_CONFIG_PATH}/
+    if [ -d "${BACKUP_DIR}/adxl_results" ]; then
+        cp -fa ${BACKUP_DIR}/adxl_results ${USER_CONFIG_PATH}/
     fi
 
-    # Copy custom user's config files from the last backup to restore them to their config directory
+    # Copy custom user's config files from the last backup to restore them to their config directory (or install templates)
+    cp -fa ${BACKUP_DIR}/*.cfg ${USER_CONFIG_PATH}
+    cp -fa ${BACKUP_DIR}/*.conf ${USER_CONFIG_PATH}
     for file in mcus.cfg overrides.cfg printer.cfg moonraker.conf variables.cfg save_variables.cfg; do
-        if [ -f "${BACKUP_PATH}/$file" ]; then
-            cp -fa ${BACKUP_PATH}/$file ${USER_CONFIG_PATH}/$file
-        else
+        if [ ! -f "${USER_CONFIG_PATH}/$file" ]; then
             cp -fa ${FRIX_CONFIG_PATH}/user_templates/$file ${USER_CONFIG_PATH}/$file
         fi
     done
@@ -85,6 +95,10 @@ function install_config {
     for file in graph_vibrations.py plot_graphs.sh; do
         chmod +x ${FRIX_CONFIG_PATH}/scripts/$file
     done
+
+    # Create or update the config version tracking file
+    cd ${FRIX_CONFIG_PATH}
+    git rev-parse HEAD > ${FRIX_CONFIG_PATH}/.VERSION
 }
 
 # Step 5: restarting Klipper
@@ -92,6 +106,9 @@ function restart_klipper {
     echo "Restarting Klipper..."
     sudo systemctl restart klipper
 }
+
+
+BACKUP_DIR="${BACKUP_PATH}/config_$(date +'%Y%m%d%H%M%S')"
 
 # Run steps
 preflight_checks
