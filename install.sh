8026427625afe020d7895efc988560b7ad026447
Benoitone <63300355+Benoitone@users.noreply.github.com>
2023-02-11 00:05:17 +0100

    ercf

--- install.sh
+++ install.sh
@@ -10,14 +10,35 @@
 
 
 # Where the user Klipper config is located (ie. the one used by Klipper to work)
+KLIPPER_PATH="${HOME}/klipper"
+KLIPPER_CONFIG_PATH="${HOME}/klipper_config"
 USER_CONFIG_PATH="$(realpath -e ${HOME}/printer_data/config)"
 # Where to clone Frix-x repository config files (read-only and keep untouched)
 FRIX_CONFIG_PATH="${HOME}/frix-x_config"
+# Where to clone ERCF-Software-V3 repository config files (read-only and keep untouched)
+ERCF_SOFTWARE_V3 _PATH="${HOME}/ERCF-Software-V3"
 # Path used to store backups when updating (backups are automatically dated when saved inside)
 BACKUP_PATH="${HOME}/frix-x_config_backups"
 
 
 set -eu
+export LC_ALL=C
+
+prompt_yn() {
+    while true; do
+        read -p "$@ (y/n)?" yn
+        case "${yn}" in
+            Y|y|Yes|yes)
+        echo "y" 
+                break;;
+            N|n|No|no)
+        echo "n" 
+                break;;
+            *)
+        ;;
+        esac
+    done
+}
 
 # Step 1: Verify that the script is not run as root and Klipper is installed
 function preflight_checks {
@@ -32,63 +53,95 @@ function preflight_checks {
         echo "Klipper service not found, please install Klipper first!"
         exit -1
     fi
+    if [ ! -d "${KLIPPER_PATH}" ]; then
+        echo -e "${ERROR}Klipper home directory (${KLIPPER_PATH}) not found."
+        exit -1
+    fi
+    if [ ! -d "${KLIPPER_CONFIG_PATH}" ]; then
+        if [ ! -d "${PRINTER_DATA_CONFIG_PATH}" ]; then
+            echo -e "${ERROR}Klipper config directory (${KLIPPER_CONFIG_PATH} or ${USER_CONFIG_PATH}) not found."
+            exit -1
+        fi
+        KLIPPER_CONFIG_PATH="${USER_CONFIG_PATH}"
+    fi
 }
 
-# Step 2: Check if the git config folder exist (or download it)
+# Step 2: Check if the git configs folders exist (or download it) if necessary
 function check_download {
-    local frixtemppath frixreponame
+    local frixtemppath frixreponame ercfrepopath ercfreponame
     frixtemppath="$(dirname ${FRIX_CONFIG_PATH})"
     frixreponame="$(basename ${FRIX_CONFIG_PATH})"
+    ercfrepopath="$(dirname ${ERCF_SOFTWARE_V3 _PATH})"
+    ercfreponame="$(basename ${ERCF_SOFTWARE_V3 _PATH})"
 
     if [ ! -d "${FRIX_CONFIG_PATH}" ]; then
         echo "Downloading Frix-x configuration folder..."
-        
         if git -C $frixtemppath clone https://github.com/Frix-x/klipper-voron-V2.git $frixreponame; then
             chmod +x ${FRIX_CONFIG_PATH}/install.sh
             echo "Download complete!"
         else
-            echo "Download of Frix-x configuration folder from https://github.com/Frix-x/klipper-voron-V2.git failed!"
+            echo "Download of Frix-x configuration git repository failed!"
             exit -1
-        fi 
+        fi
     else
-        echo "Frix-x configuration folder found!"
+        echo "Frix-x git repository folder found locally!"
     fi
+
+    echo
+    echo -e "Do you use an ERCF and do you want to use ERCF Software V3...${INPUT}"
+    echo
+    case $yn in
+        y)
+            if [ ! -d "${ERCF_SOFTWARE_V3 _PATH}" ]; then
+                echo "Downloading ERCF Software V3 configuration folder..."
+                if git -C $ercfrepopath clone https://github.com/moggieuk/ERCF-Software-V3.git $ercfreponame;
+                    echo "Download complete!"
+                else
+                    echo "Download of ERCF Software V3 configuration git repository failed!"
+                    exit -1
+                fi
+            else
+                echo "ERCF Software V3 git repository folder found locally!"
+            fi
+            ;;
+        n)
+            exit -1
+    esac
 }
 
 # Step 3: Backup the old Klipper configuration
 function backup_config {
-    echo "Backup your old configuration files... You will find them in: ${BACKUP_DIR}"
-    if [ ! -d "${BACKUP_PATH}" ]; then
-        mkdir ${BACKUP_PATH}
+    mkdir -p ${BACKUP_DIR}
+
+    if [ -f "${USER_CONFIG_PATH}/.VERSION" ]; then
+        echo "Frix-x configuration already in use: only a backup of the custom user cfg files is needed"
+        find ${USER_CONFIG_PATH} -type f -regex '.*\.\(cfg\|conf\|VERSION\)' | xargs mv -t ${BACKUP_DIR}/ 2>/dev/null
+    else
+        echo "New installation detected: a full backup of the user config folder is needed"
+        cp -fa ${USER_CONFIG_PATH} ${BACKUP_DIR}
     fi
-    mv -f ${USER_CONFIG_PATH}/*.cfg ${BACKUP_DIR}
-    mv -f ${USER_CONFIG_PATH}/*.conf ${BACKUP_DIR}
-    mv -f ${USER_CONFIG_PATH}/.VERSION ${BACKUP_DIR}
+
+    echo "Backup done in: ${BACKUP_DIR}"
 }
 
 # Step 4: Put the new configuration files in place to be ready to start
 function install_config {
-    echo "Installing the new Frix-x Klipper configuration..."
+    echo "Installation of the last Frix-x Klipper configuration files"
     mkdir -p ${USER_CONFIG_PATH}
 
-    # Symlink Frix-x config folders (read-only repository) to the user's config directory
+    # Symlink Frix-x config folders (read-only git repository) to the user's config directory
     for dir in config macros scripts moonraker; do
-        ln -fs ${FRIX_CONFIG_PATH}/$dir ${USER_CONFIG_PATH}/$dir
+        ln -fsn ${FRIX_CONFIG_PATH}/$dir ${USER_CONFIG_PATH}/$dir
     done
 
-    # Copy the ADXL results from the last backup to restore them to the user's config directory
-    if [ -d "${BACKUP_DIR}/adxl_results" ]; then
-        cp -fa ${BACKUP_DIR}/adxl_results ${USER_CONFIG_PATH}/
+    # Copy custom user's config files from the last backup to restore them to their config directory (or install templates if it's a first install)
+    if [ -f "${BACKUP_DIR}/.VERSION" ]; then
+        echo "Update done: restoring user config files now!"
+        find ${BACKUP_DIR} -type f -regex '.*\.\(cfg\|conf\)' | xargs cp -ft ${USER_CONFIG_PATH}/
+    else
+        echo "New installation detected: default config templates will be set in place!"
+        cp -fa ${FRIX_CONFIG_PATH}/user_templates/* ${USER_CONFIG_PATH}/
     fi
-
-    # Copy custom user's config files from the last backup to restore them to their config directory (or install templates)
-    cp -fa ${BACKUP_DIR}/*.cfg ${USER_CONFIG_PATH}
-    cp -fa ${BACKUP_DIR}/*.conf ${USER_CONFIG_PATH}
-    for file in mcus.cfg overrides.cfg printer.cfg moonraker.conf variables.cfg save_variables.cfg; do
-        if [ ! -f "${USER_CONFIG_PATH}/$file" ]; then
-            cp -fa ${FRIX_CONFIG_PATH}/user_templates/$file ${USER_CONFIG_PATH}/$file
-        fi
-    done
 
     # CHMOD the scripts to be sure they are all executables (Git should keep the modes on files but it's to be sure)
     chmod +x ${FRIX_CONFIG_PATH}/install.sh
@@ -96,12 +149,25 @@ function install_config {
         chmod +x ${FRIX_CONFIG_PATH}/scripts/$file
     done
 
-    # Create or update the config version tracking file
-    cd ${FRIX_CONFIG_PATH}
-    git rev-parse HEAD > ${FRIX_CONFIG_PATH}/.VERSION
+    # Create the config version tracking file in the user config directory
+    git -C ${FRIX_CONFIG_PATH} rev-parse HEAD > ${USER_CONFIG_PATH}/.VERSION
 }
 
-# Step 5: restarting Klipper
+# Step 5: Put the ercf plugin files in place to be ready to use if ERCF Software V3
+function link_ercf_plugin() {
+    if [ -d "${ERCF_SOFTWARE_V3 _PATH}" ]; then
+        echo -e "Linking ercf extension to Klipper..."
+        if [ -d "${KLIPPER_HOME}/klippy/extras" ]; then
+            for file in `cd ${ERCF_SOFTWARE_V3 _PATH}/extras ; ls *.py`; do
+                ln -sf "${ERCF_SOFTWARE_V3 _PATH}/extras/${file}" "${KLIPPER_HOME}/klippy/extras/${file}"
+            done
+        else
+            echo -e "ERCF modules not installed because Klipper 'extras' directory not found!"
+        fi
+    fi
+}
+
+# Step 6: restarting Klipper
 function restart_klipper {
     echo "Restarting Klipper..."
     sudo systemctl restart klipper
@@ -115,4 +181,7 @@ preflight_checks
 check_download
 backup_config
 install_config
+link_ercf_plugin
 restart_klipper
+
+echo "Everything is ok, Frix-x config installed and up to date!"
