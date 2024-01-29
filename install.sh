a25830a105035cc77725cc05d464b0441c2dfd44
Benoitone <63300355+Benoitone@users.noreply.github.com>
2024-01-29 23:42:21 +0100

    change branch install dev_B

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ BACKUP_PATH="${HOME}/klippain_config_backups"
 # Where the Klipper folder is located (ie. the internal Klipper firmware machinery)
 KLIPPER_PATH="${HOME}/klipper"
 # Branch on Frix-x repo to use (default: main)
-FRIX_BRANCH="Happy_Hare_Dev"
+FRIX_BRANCH="HHv2_Dev_B"
 
 
 set -eu
