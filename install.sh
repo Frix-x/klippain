593e74e277e99f2650bae6f3e68d4088f9740ffe
claudioguareschi <33001685+claudioguareschi@users.noreply.github.com>
2024-02-07 19:21:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ function check_download {
 
     if [ ! -d "${FRIX_CONFIG_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain repository..."
-        if git -C $frixtemppath clone -b $frixbranchname https://github.com/Frix-x/klippain.git $frixreponame; then
+        if git -C $frixtemppath clone -b $frixbranchname https://github.com/claudioguareschi/klippain.git $frixreponame; then
             chmod +x ${FRIX_CONFIG_PATH}/install.sh
             printf "[DOWNLOAD] Download complete!\n\n"
         else
